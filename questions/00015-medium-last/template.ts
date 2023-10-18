ac94912ffd3326dfb9b62d51dbab7755f994320c
Kawano Kenichi <kenichi.k.820@gmail.com>
2023-10-18 13:35:39 +0000

    ✨ Add an answer, 00015-medium-last

--- questions/00015-medium-last/template.ts
+++ questions/00015-medium-last/template.ts
@@ -1,1 +1,7 @@
-type Last<T extends any[]> = any
+type Tail<T extends unknown[]> = ((...as: T) => never) extends (b: never, ...bs: infer U) => never ? U : never;
+
+// 配列を縮小する方針
+type Last<T extends any[]> = T[Tail<T>["length"]];
+
+// 配列を拡張する方針
+// type Last<T extends any[]> = [never, ...T][T["length"]]
