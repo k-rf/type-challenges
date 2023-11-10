346f8f3ed3bf323cfe31bfd79696181ac3d8d856
Kawano Kenichi <kenichi.k.820@gmail.com>
2023-11-10 04:50:09 +0000

    🎨 Update an answer, 00527-medium-append-to-object

--- questions/00527-medium-append-to-object/template.ts
+++ questions/00527-medium-append-to-object/template.ts
@@ -1,1 +1,1 @@
-type AppendToObject<T, U extends PropertyKey, V> = { [K in keyof (T & Record<U, V>)]: (T & Record<U, V>)[K] };
+type AppendToObject<T, U extends PropertyKey, V> = { [K in keyof T | U]: K extends keyof T ? T[K] : V };
