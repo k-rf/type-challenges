70f4d389433098453191fc706e45736b9c7b3eee
Kawano Kenichi <kenichi.k.820@gmail.com>
2023-12-06 10:44:15 +0000

    🎨 Update an answer, 00599-medium-merge

--- questions/00599-medium-merge/template.ts
+++ questions/00599-medium-merge/template.ts
@@ -1,2 +1,1 @@
-type Override<F, S> = { [K in keyof F as K extends keyof S ? never : K]: F[K] } & S;
-type Merge<F, S> = { [K in keyof Override<F, S>]: Override<F, S>[K] };
+type Merge<F, S, O = F & S> = { [K in keyof O]: K extends keyof S ? S[K] : O[K] };
