e7382ef9503c7bc43a3f9d0d45d086ed72dd07a1
Kawano Kenichi <kenichi.k.820@gmail.com>
2024-03-03 15:36:44 +0000

    🎨 Update an answer, 00612-medium-kebabcase

--- questions/00612-medium-kebabcase/template.ts
+++ questions/00612-medium-kebabcase/template.ts
@@ -1,6 +1,5 @@
-type Skewer<S extends string> = S extends `${infer U}${infer R}`
-  ? U extends Lowercase<U>
-    ? `${U}${Skewer<R>}`
-    : `-${Lowercase<U>}${Skewer<R>}`
-  : ''
-type KebabCase<S extends string> = Skewer<S> extends `-${infer U}` ? U extends '' ? '-' : U : Skewer<S>
+type KebabCase<S extends string> = S extends `${infer U}${infer R}`
+  ? R extends Uncapitalize<R>
+    ? `${Uncapitalize<U>}${KebabCase<R>}`
+    : `${Uncapitalize<U>}-${KebabCase<R>}`
+  : S
