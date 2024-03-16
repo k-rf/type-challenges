a408ea9a893029a6766158e7b25a0a44cc1c53c2
Kawano Kenichi <kenichi.k.820@gmail.com>
2024-03-16 11:15:03 +0000

    ✨ Add an answer, 00949-medium-anyof

--- questions/00949-medium-anyof/template.ts
+++ questions/00949-medium-anyof/template.ts
@@ -1,1 +1,7 @@
-type AnyOf<T extends readonly any[]> = any
+type Falsy = false | 0 | '' | undefined | null | [] | Record<PropertyKey, undefined>
+
+type AnyOf<T extends readonly any[]> = T['length'] extends 0
+  ? false
+  : T extends [infer U, ...infer V]
+    ? U extends Falsy ? AnyOf<V> : true
+    : true
