49b7dd4a833c090263a36923dfb24ed7d7329f19
Kawano Kenichi <kenichi.k.820@gmail.com>
2024-04-24 15:10:46 +0000

    ✨ Add an answer, 01978-medium-percentage-parser

--- questions/01978-medium-percentage-parser/template.ts
+++ questions/01978-medium-percentage-parser/template.ts
@@ -1,1 +1,16 @@
-type PercentageParser<A extends string> = any
+type SignMatcher<
+  A extends string,
+  Sign extends string = "+" | "-",
+> = A extends `${infer S extends Sign}${infer R}` ? [S, R] : ["", A];
+
+type UnitMatcher<
+  A extends string,
+  Unit extends string = "%",
+> = A extends `${infer R}${Unit}`
+  ? [R, A extends `${R}${infer U}` ? U : ""]
+  : [A, ""];
+
+type PercentageParser<A extends string> = [
+  SignMatcher<A>[0],
+  ...UnitMatcher<SignMatcher<A>[1]>,
+];
