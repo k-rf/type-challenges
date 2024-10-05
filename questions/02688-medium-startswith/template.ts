cb2aaa85ace85e2ff86ef89b9f5353bc737927bc
Kawano Kenichi <kenichi.k.820@gmail.com>
2024-10-05 11:18:11 +0000

    🎨 Update an answer, 02688-medium-startswith

--- questions/02688-medium-startswith/template.ts
+++ questions/02688-medium-startswith/template.ts
@@ -1,10 +1,3 @@
-type StartsWith<T extends string, U extends string> = [T, U] extends [
-  string,
-  "",
-]
+type StartsWith<T extends string, U extends string> = T extends `${U}${string}`
   ? true
-  : [T, U] extends [`${infer T1}${infer TR}`, `${infer U1}${infer UR}`]
-  ? T1 extends U1
-    ? StartsWith<TR, UR>
-    : false
   : false;
