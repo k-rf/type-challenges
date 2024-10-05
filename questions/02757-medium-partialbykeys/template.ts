605f87ca4464c4ab693f7f58ce90b86e3afbdac4
Kawano Kenichi <kenichi.k.820@gmail.com>
2024-10-05 11:18:11 +0000

    ✨ Add an answer, 02757-medium-partialbykeys

--- questions/02757-medium-partialbykeys/template.ts
+++ questions/02757-medium-partialbykeys/template.ts
@@ -1,1 +1,7 @@
-type PartialByKeys<T, K> = any
+type Prettify<T> = { [K in keyof T]: T[K] } & {};
+
+type PartialByKeys<T, K extends keyof T = keyof T> = Prettify<
+  Partial<T> & {
+    [P in keyof T as P extends K ? never : P]: T[P];
+  }
+>;
