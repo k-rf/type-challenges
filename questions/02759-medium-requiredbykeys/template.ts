d39632be7b46670e34d9f60d6523167d3f15d51f
Kawano Kenichi <kenichi.k.820@gmail.com>
2024-10-23 16:11:56 +0000

    ✨ Add an answer, 02759-medium-requiredbykeys

--- questions/02759-medium-requiredbykeys/template.ts
+++ questions/02759-medium-requiredbykeys/template.ts
@@ -1,1 +1,7 @@
-type RequiredByKeys<T, K> = any
+type RequiredByKeys<T, K extends keyof T = keyof T> = Composite<
+  {
+    [P in keyof T as IncludeKey<P, K>]-?: T[P];
+  } & {
+    [P in keyof T as ExcludeKey<P, K>]: T[P];
+  }
+>;
