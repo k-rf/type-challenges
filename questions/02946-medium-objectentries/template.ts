aeca288049c59ba31840b76382b6c464dffaad26
Kawano Kenichi <kenichi.k.820@gmail.com>
2024-10-23 17:05:13 +0000

    ✨ Add an answer, 02946-medium-objectentries

--- questions/02946-medium-objectentries/template.ts
+++ questions/02946-medium-objectentries/template.ts
@@ -1,1 +1,7 @@
-type ObjectEntries<T> = any
+type ObjectEntries<T> = T extends { [K in keyof T]-?: T[K] | undefined }
+  ? { [K in keyof T]: [K, T[K]] }[keyof T]
+  : {
+      [K in keyof T]-?: T[K] extends infer U | undefined
+        ? [K, U]
+        : [K, keyof T];
+    }[keyof T];
