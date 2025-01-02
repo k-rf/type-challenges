a165da750573feb4b614f0e4fc144b7ad521e573
Kawano Kenichi <kenichi.k.820@gmail.com>
2025-01-02 14:09:02 +0000

    ✨ Add an answer, 03192-medium-reverse

--- questions/03192-medium-reverse/template.ts
+++ questions/03192-medium-reverse/template.ts
@@ -1,1 +1,5 @@
-type Reverse<T> = any
+type Reverse<T extends unknown[], Acc extends unknown[] = []> = T extends []
+  ? Acc
+  : T extends [infer U, ...infer R]
+  ? Reverse<R, [U, ...Acc]>
+  : never;
