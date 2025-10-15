8e669382519a125ee6e661aacd06c99bd0627959
Kawano Kenichi <kenichi.k.820@gmail.com>
2025-10-15 15:39:24 +0000

    ✨ Add an answer, 03326-medium-bem-style-string

--- questions/03326-medium-bem-style-string/template.ts
+++ questions/03326-medium-bem-style-string/template.ts
@@ -1,1 +1,9 @@
-type BEM<B extends string, E extends string[], M extends string[]> = any
+type BEMHelper<P extends string, T extends string[]> = T extends []
+  ? ""
+  : `${P}${T[number]}`;
+
+type BEM<
+  B extends string,
+  E extends string[],
+  M extends string[],
+> = `${B}${BEMHelper<"__", E>}${BEMHelper<"--", M>}`;
