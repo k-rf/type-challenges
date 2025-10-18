ea0c7705d8e468eea1b599d160d02c759c079310
Kawano Kenichi <kenichi.k.820@gmail.com>
2025-10-18 16:39:57 +0000

    ✨ Add an answer, 04182-medium-fibonacci-sequence

--- questions/04182-medium-fibonacci-sequence/template.ts
+++ questions/04182-medium-fibonacci-sequence/template.ts
@@ -1,1 +1,8 @@
-type Fibonacci<T extends number> = any
+type Fibonacci<
+  T extends number,
+  F1 extends number[] = [0],
+  F2 extends number[] = [0],
+  C extends number[] = [0],
+> = T extends C["length"]
+  ? F1["length"]
+  : Fibonacci<T, F2, [...F1, ...F2], [...C, 0]>;
