095684ff0214235f0a95eb9bedc0fe87f330b0d1
Kawano Kenichi <kenichi.k.820@gmail.com>
2025-10-20 17:19:07 +0000

    ✨ Add an answer, 04260-medium-nomiwase

--- questions/04260-medium-nomiwase/template.ts
+++ questions/04260-medium-nomiwase/template.ts
@@ -1,1 +1,13 @@
-type AllCombinations<S> = any
+type Helper<S extends string> = S extends `${infer U}${infer V}`
+  ? U | Helper<V>
+  : S;
+
+type AllCombinations<S extends string, U extends string = Helper<S>> = [
+  U,
+] extends [never]
+  ? ""
+  :
+      | ""
+      | {
+          [K in U]: `${K}${AllCombinations<never, Exclude<U, K>>}`;
+        }[U];
