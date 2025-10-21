6ea1dd1116c3e48e6c4694b0cadc83759350a37f
Kawano Kenichi <kenichi.k.820@gmail.com>
2025-10-21 16:44:04 +0000

    ✨ Add an answer, 04425-medium-greater-than

--- questions/04425-medium-greater-than/template.ts
+++ questions/04425-medium-greater-than/template.ts
@@ -1,1 +1,19 @@
-type GreaterThan<T extends number, U extends number> = any
+type GreaterThan<
+  T extends number | string,
+  U extends number | string,
+  Res = false,
+> = `${T}` extends `${infer TF}${infer TR}`
+  ? `${U}` extends `${infer UF}${infer UR}`
+    ? [Res, TF & UF] extends [false, never] // Res == false and TF != UF
+      ? GreaterThan<
+          TR,
+          UR,
+          "0123456789" extends `${string}${TF}${string}${UF}${string}`
+            ? false
+            : true
+        >
+      : GreaterThan<TR, UR, Res>
+    : true
+  : U extends ""
+  ? Res
+  : false;
