a349e849279dc1da1eda1f7a98eeec438876e6ab
Kawano Kenichi <kenichi.k.820@gmail.com>
2025-10-22 18:03:49 +0000

    ✨ Add an answer, 04471-medium-zip

--- questions/04471-medium-zip/template.ts
+++ questions/04471-medium-zip/template.ts
@@ -1,1 +1,6 @@
-type Zip<T, U> = any
+type Zip<T extends unknown[], U extends unknown[]> = [T, U] extends [
+  [infer X, ...infer RX],
+  [infer Y, ...infer RY],
+]
+  ? [[X, Y], ...Zip<RX, RY>]
+  : [];
