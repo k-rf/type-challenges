646993e9d8fbb076d3381127846ee0f3a6d39b3a
Kawano Kenichi <kenichi.k.820@gmail.com>
2025-10-25 15:16:17 +0000

    ✨ Add an answer, 04518-medium-fill

--- questions/04518-medium-fill/template.ts
+++ questions/04518-medium-fill/template.ts
@@ -2,5 +2,18 @@ type Fill<
   T extends unknown[],
   N,
   Start extends number = 0,
-  End extends number = T['length'],
-> = any
+  End extends number = T["length"],
+  Acc extends unknown[] = [],
+> = T extends [infer U, ...infer R]
+  ? Fill<
+      R,
+      N,
+      Start,
+      End,
+      GreaterThan<Start, Acc["length"]> extends true
+        ? [...Acc, U]
+        : GreaterThan<End, Acc["length"]> extends true
+        ? [...Acc, N]
+        : [...Acc, U]
+    >
+  : Acc;
