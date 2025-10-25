c777a26d5098e34ee5fb7791f99e2db16bd77a47
Kawano Kenichi <kenichi.k.820@gmail.com>
2025-10-25 15:15:57 +0000

    🎨 Update an answer, 04499-medium-chunk

--- questions/04499-medium-chunk/template.ts
+++ questions/04499-medium-chunk/template.ts
@@ -1,15 +1,11 @@
-type Head<
+type Chunk<
   T extends readonly unknown[],
   N extends number,
-  R extends unknown[] = [],
-> = R["length"] extends N
-  ? R
-  : T extends [infer U, ...infer V]
-  ? Head<V, N, [...R, U]>
-  : R;
-
-type Chunk<T extends readonly unknown[], N extends number> = T extends []
-  ? T
-  : T extends [...Head<T, N>, ...infer R]
-  ? [Head<T, N>, ...Chunk<R, N>]
-  : [];
+  Acc extends unknown[] = [],
+> = Acc["length"] extends N
+  ? [Acc, ...Chunk<T, N>]
+  : T extends [infer K, ...infer L]
+  ? Chunk<L, N, [...Acc, K]>
+  : Acc extends []
+  ? Acc
+  : [Acc];
