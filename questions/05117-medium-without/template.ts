b7a6c989c48e6387206e937f4d73407f732e4d8b
Kawano Kenichi <kenichi.k.820@gmail.com>
2025-10-28 17:08:33 +0000

    ✨ Add an answer, 05117-medium-without

--- questions/05117-medium-without/template.ts
+++ questions/05117-medium-without/template.ts
@@ -1,1 +1,3 @@
-type Without<T, U> = any
+type Without<T, U, Acc extends unknown[] = []> = T extends [infer X, ...infer Y]
+  ? Without<Y, U, X extends Flatten<[U]>[number] ? Acc : [...Acc, X]>
+  : Acc;
