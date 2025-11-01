5d91ae67cc7b05039cc98783391f74357beebd1f
Kawano Kenichi <kenichi.k.820@gmail.com>
2025-11-01 07:48:31 +0000

    ✨ Add an answer, 05310-medium-join

--- questions/05310-medium-join/template.ts
+++ questions/05310-medium-join/template.ts
@@ -1,1 +1,7 @@
-type Join<T, U> = any
+type Join<
+  T extends string[],
+  U extends string | number = ",",
+  Acc extends string = "",
+> = T extends [infer S extends string, ...infer R extends string[]]
+  ? Join<R, U, Acc extends "" ? S : `${Acc}${U}${S}`>
+  : Acc;
