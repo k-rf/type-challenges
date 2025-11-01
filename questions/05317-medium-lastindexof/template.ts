cd77ca95d448a77b04a8844360426add8ed0e923
Kawano Kenichi <kenichi.k.820@gmail.com>
2025-11-01 07:48:48 +0000

    ✨ Add an answer, 05317-medium-lastindexof

--- questions/05317-medium-lastindexof/template.ts
+++ questions/05317-medium-lastindexof/template.ts
@@ -1,1 +1,9 @@
-type LastIndexOf<T, U> = any
+type LastIndexOf<
+  T extends unknown[],
+  U,
+  C extends unknown[] = T extends [infer _, ...infer R] ? R : [],
+> = T extends [...infer R, infer X]
+  ? (<T>() => T extends X ? 1 : 2) extends <T>() => T extends U ? 1 : 2
+    ? C["length"]
+    : LastIndexOf<R, U, C extends [infer _, ...infer Rest] ? Rest : []>
+  : -1;
