7f7effe585b0280e0ce12ae0a6a8abab754fd840
Kawano Kenichi <kenichi.k.820@gmail.com>
2025-11-03 16:22:28 +0000

    ✨ Add an answer, 05821-medium-maptypes

--- questions/05821-medium-maptypes/template.ts
+++ questions/05821-medium-maptypes/template.ts
@@ -1,1 +1,9 @@
-type MapTypes<T, R> = any
+type FromTo = { mapFrom: unknown; mapTo: unknown };
+
+type MapToOf<T, R extends FromTo> = R extends { mapFrom: T }
+  ? R["mapTo"]
+  : never;
+
+type MapTypes<T extends Record<PropertyKey, unknown>, R extends FromTo> = {
+  [K in keyof T]: T[K] extends R["mapFrom"] ? MapToOf<T[K], R> : T[K];
+};
