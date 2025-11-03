72e66261dc70c9ceef11e46afc6c76758c3cf3dc
Kawano Kenichi <kenichi.k.820@gmail.com>
2025-11-03 15:59:28 +0000

    ✨ Add an answer, 05360-medium-unique

--- questions/05360-medium-unique/template.ts
+++ questions/05360-medium-unique/template.ts
@@ -1,1 +1,6 @@
-type Unique<T> = any
+type Unique<T extends unknown[], Acc extends unknown[] = []> = T extends [
+  infer U,
+  ...infer R,
+]
+  ? Unique<R, Includes<Acc, U> extends true ? Acc : [...Acc, U]>
+  : Acc;
