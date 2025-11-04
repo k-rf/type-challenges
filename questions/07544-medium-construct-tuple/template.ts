6ef8995d205670a69c5b4fb3b907582304a3a761
Kawano Kenichi <kenichi.k.820@gmail.com>
2025-11-04 15:45:08 +0000

    ✨ Add an answer, 07544-medium-construct-tuple

--- questions/07544-medium-construct-tuple/template.ts
+++ questions/07544-medium-construct-tuple/template.ts
@@ -1,1 +1,4 @@
-type ConstructTuple<L extends number> = any
+type ConstructTuple<
+  L extends number,
+  Acc extends unknown[] = [],
+> = L extends Acc["length"] ? Acc : ConstructTuple<L, [...Acc, unknown]>;
