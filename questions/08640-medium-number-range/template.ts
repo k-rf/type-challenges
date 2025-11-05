3606f370f70dca09690af532fc36d38d05bfe08a
Kawano Kenichi <kenichi.k.820@gmail.com>
2025-11-05 15:57:31 +0000

    🎨 Update an answer, 08640-medium-number-range
    AIにリファクタリングさせると気持ちの良いコードが出力された。

--- questions/08640-medium-number-range/template.ts
+++ questions/08640-medium-number-range/template.ts
@@ -1,8 +1,16 @@
-type NumberRange<
-  L extends number,
-  H extends number,
-  Acc extends unknown[] = ConstructTuple<L>,
-  Ans extends number[] = [Acc["length"]],
-> = Acc["length"] extends H
-  ? Ans[number]
-  : NumberRange<L, H, [...Acc, unknown], [...Ans, [...Acc, unknown]["length"]]>;
+// Inc<N>: N を +1 する（型レベル加算）
+type Inc<N extends number, T extends unknown[] = []> = T["length"] extends N
+  ? [...T, unknown]["length"]
+  : Inc<N, [...T, unknown]>;
+
+// Enumerate<N>: 0..N-1 のユニオン
+type Enumerate<
+  N extends number,
+  T extends number[] = [],
+> = T["length"] extends N ? T[number] : Enumerate<N, [...T, T["length"]]>;
+
+// Range[L, H] = Enumerate<H+1> - Enumerate<L> = L..H
+type NumberRange<L extends number, H extends number> = Exclude<
+  Enumerate<Inc<H>>,
+  Enumerate<L>
+>;
