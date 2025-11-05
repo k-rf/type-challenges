dcdbfdea595b4d53dac6392f6413cf677dc6caf2
Kawano Kenichi <kenichi.k.820@gmail.com>
2025-11-05 15:48:57 +0000

    ✨ Add an answer, 08640-medium-number-range

--- questions/08640-medium-number-range/template.ts
+++ questions/08640-medium-number-range/template.ts
@@ -1,1 +1,8 @@
-type NumberRange<L, H> = any
+type NumberRange<
+  L extends number,
+  H extends number,
+  Acc extends unknown[] = ConstructTuple<L>,
+  Ans extends number[] = [Acc["length"]],
+> = Acc["length"] extends H
+  ? Ans[number]
+  : NumberRange<L, H, [...Acc, unknown], [...Ans, [...Acc, unknown]["length"]]>;
