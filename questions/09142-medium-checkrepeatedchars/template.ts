d0c822e430c7f95435037f4f82bffa44352f9632
Kawano Kenichi <kenichi.k.820@gmail.com>
2025-11-08 14:12:20 +0000

    ✨ Add an answer, 09142-medium-checkrepeatedchars

--- questions/09142-medium-checkrepeatedchars/template.ts
+++ questions/09142-medium-checkrepeatedchars/template.ts
@@ -1,1 +1,9 @@
-type CheckRepeatedChars<T extends string> = any
+type IsNotEqual<X, Y> = IsEqual<X, Y> extends true ? false : true;
+
+type CheckRepeatedChars<
+  T extends string,
+  S extends string[] = ToSplitted<T>,
+  Acc extends string[] = [],
+> = T extends `${infer U}${infer R}`
+  ? CheckRepeatedChars<R, S, Includes<Acc, U> extends true ? Acc : [...Acc, U]>
+  : IsNotEqual<Acc["length"], S["length"]>;
