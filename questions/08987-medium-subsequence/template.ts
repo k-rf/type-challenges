ee5f75fdd3da9c9617da61782e5cb9eaa4db930f
Kawano Kenichi <kenichi.k.820@gmail.com>
2025-11-08 10:05:04 +0000

    ✨ Add an answer, 08987-medium-subsequence

--- questions/08987-medium-subsequence/template.ts
+++ questions/08987-medium-subsequence/template.ts
@@ -1,1 +1,6 @@
-type Subsequence<T extends any[]> = any
+type Subsequence<T extends any[], Ans extends any[] = []> = T extends [
+  infer U,
+  ...infer R,
+]
+  ? Subsequence<R, Ans | [...Ans, U]>
+  : Ans;
