8196a420fe591f9f0b373e1dfd77218bf654d4c0
Kawano Kenichi <kenichi.k.820@gmail.com>
2025-11-10 17:28:13 +0000

    ✨ Add an answer, 09286-medium-firstuniquecharindex
    わからなかった…

--- questions/09286-medium-firstuniquecharindex/template.ts
+++ questions/09286-medium-firstuniquecharindex/template.ts
@@ -1,1 +1,12 @@
-type FirstUniqueCharIndex<T extends string> = any
+type FirstUniqueCharIndex<
+  T extends string,
+  Ans extends string[] = [],
+> = T extends ""
+  ? -1
+  : T extends `${infer H}${infer R}`
+  ? H extends Ans[number]
+    ? FirstUniqueCharIndex<R, [...Ans, H]>
+    : R extends `${string}${H}${string}`
+    ? FirstUniqueCharIndex<R, [...Ans, H]>
+    : Ans["length"]
+  : never;
