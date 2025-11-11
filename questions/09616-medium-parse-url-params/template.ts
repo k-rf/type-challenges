a0409aaad286931ba90ddd884f0638c510d933a2
Kawano Kenichi <kenichi.k.820@gmail.com>
2025-11-11 14:33:47 +0000

    ✨ Add an answer, 09616-medium-parse-url-params

--- questions/09616-medium-parse-url-params/template.ts
+++ questions/09616-medium-parse-url-params/template.ts
@@ -1,1 +1,8 @@
-type ParseUrlParams<T> = any
+type ParseUrlParams<
+  T extends string,
+  Ans extends string[] = [],
+> = T extends `${string}:${infer U}/${infer R}`
+  ? ParseUrlParams<R, [...Ans, U]>
+  : T extends `${string}:${infer U}`
+  ? ParseUrlParams<"", [...Ans, U]>
+  : Ans[number];
