4656cb295176b9714306551ed4b0534825c841d3
Kawano Kenichi <kenichi.k.820@gmail.com>
2025-11-11 14:39:08 +0000

    🎨 Update an answer, 09616-medium-parse-url-params
    最適化

--- questions/09616-medium-parse-url-params/template.ts
+++ questions/09616-medium-parse-url-params/template.ts
@@ -1,8 +1,5 @@
-type ParseUrlParams<
-  T extends string,
-  Ans extends string[] = [],
-> = T extends `${string}:${infer U}/${infer R}`
-  ? ParseUrlParams<R, [...Ans, U]>
-  : T extends `${string}:${infer U}`
-  ? ParseUrlParams<"", [...Ans, U]>
-  : Ans[number];
+type ParseUrlParams<T extends string> = T extends `${string}:${infer U}`
+  ? U extends `${infer V}/${infer R}`
+    ? V | ParseUrlParams<R>
+    : U
+  : never;
