25f0bb86dca482cf8fa44a740fa2a51d3d7bfb29
Kawano Kenichi <kenichi.k.820@gmail.com>
2025-11-16 06:57:29 +0000

    ✨ Add an answer, 09898-medium-zhao-chu-mu-biao-shu-zu-zhong-zhi-chu-xian-guo-yi-ci-de-yuan-su

--- questions/09898-medium-zhao-chu-mu-biao-shu-zu-zhong-zhi-chu-xian-guo-yi-ci-de-yuan-su/template.ts
+++ questions/09898-medium-zhao-chu-mu-biao-shu-zu-zhong-zhi-chu-xian-guo-yi-ci-de-yuan-su/template.ts
@@ -1,1 +1,13 @@
-type FindEles<T extends any[]> = any
+type FindEles<
+  T extends any[],
+  Ans extends unknown[] = [],
+  Check extends unknown[] = [],
+> = T extends [infer U, ...infer R]
+  ? Includes<R, U> extends false
+    ? FindEles<
+        R,
+        Includes<Check, U> extends true ? Ans : [...Ans, U],
+        [...Check, U]
+      >
+    : FindEles<R, Ans, [...Check, U]>
+  : Ans;
