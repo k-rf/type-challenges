ff262f189ca5bd1d988bf6cbf2fdbf8540b6ce15
Kawano Kenichi <kenichi.k.820@gmail.com>
2025-11-19 16:15:45 +0000

    ✨ Add an answer, 16259-medium-to-primitive

--- questions/16259-medium-to-primitive/template.ts
+++ questions/16259-medium-to-primitive/template.ts
@@ -1,1 +1,7 @@
-type ToPrimitive = any
+type ToPrimitive<T> = T extends object
+  ? T extends Function
+    ? Function
+    : { [K in keyof T]: ToPrimitive<T[K]> }
+  : T extends { valueOf: () => infer U }
+  ? U
+  : T;
