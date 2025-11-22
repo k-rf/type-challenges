6775cb52356b69dd922e73daeb155a1e5e9256c0
Kawano Kenichi <kenichi.k.820@gmail.com>
2025-11-22 06:44:06 +0000

    ✨ Add an answer, 18220-medium-filter

--- questions/18220-medium-filter/template.ts
+++ questions/18220-medium-filter/template.ts
@@ -1,1 +1,5 @@
-type Filter<T extends any[], P> = []
+type Filter<T extends any[], P> = T extends [infer U, ...infer R]
+  ? U extends P
+    ? [U, ...Filter<R, P>]
+    : [...Filter<R, P>]
+  : [];
