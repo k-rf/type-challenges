bcb18a6cf38cdda6638035206937cde3a472b83b
Kawano Kenichi <kenichi.k.820@gmail.com>
2025-11-24 06:08:36 +0000

    ✨ Add an answer, 21104-medium-findall

--- questions/21104-medium-findall/template.ts
+++ questions/21104-medium-findall/template.ts
@@ -1,1 +1,22 @@
-type FindAll<T extends string, P extends string> = any
+type FindAll<
+  T extends string,
+  P extends string,
+  Offset extends string[] = [],
+> = P extends ""
+  ? []
+  : T extends `${infer L}${P}${infer R}`
+  ? [
+      [...Offset, ...ToSplitted<L>]["length"],
+      ...FindAll<
+        `${Join<Tail<ToSplitted<P>>>}${R}`,
+        P,
+        [
+          ...Offset,
+          ...ToSplitted<L>,
+          ...(ToSplitted<P>["length"] extends 1
+            ? ToSplitted<P>
+            : Tail<ToSplitted<P>>),
+        ]
+      >,
+    ]
+  : [];
