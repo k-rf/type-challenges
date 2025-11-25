2621948a9ab12894540e4650b8bed832872eda5c
Kawano Kenichi <kenichi.k.820@gmail.com>
2025-11-25 15:30:08 +0000

    ✨ Add an answer, 21106-medium-zu-he-jian-lei-xing-combination-key-type

--- questions/21106-medium-zu-he-jian-lei-xing-combination-key-type/template.ts
+++ questions/21106-medium-zu-he-jian-lei-xing-combination-key-type/template.ts
@@ -1,2 +1,6 @@
-// 实现 Combs
-type Combs<T extends any[]> = any
+type Combs<T extends any[]> = T extends [
+  infer U extends string,
+  ...infer R extends string[],
+]
+  ? `${U} ${R[number]}` | Combs<R>
+  : never;
