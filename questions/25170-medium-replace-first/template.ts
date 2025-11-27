96dd83d7a09ba33945d3068f336ee1d33bbb07e3
Kawano Kenichi <kenichi.k.820@gmail.com>
2025-11-27 16:35:45 +0000

    ✨ Add an answer, 25170-medium-replace-first

--- questions/25170-medium-replace-first/template.ts
+++ questions/25170-medium-replace-first/template.ts
@@ -1,1 +1,8 @@
-type ReplaceFirst<T extends readonly unknown[], S, R> = any
+type ReplaceFirst<T extends readonly unknown[], S, R> = T extends [
+  infer U,
+  ...infer Rest,
+]
+  ? U extends S
+    ? [R, ...Rest]
+    : [U, ...ReplaceFirst<Rest, S, R>]
+  : [];
