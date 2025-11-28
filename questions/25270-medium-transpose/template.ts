fcd8427ce0061191de42a8d76bf8505815402b01
Kawano Kenichi <kenichi.k.820@gmail.com>
2025-11-28 18:05:54 +0000

    ✨ Add an answer, 25270-medium-transpose

--- questions/25270-medium-transpose/template.ts
+++ questions/25270-medium-transpose/template.ts
@@ -1,1 +1,20 @@
-type Transpose<M extends number[][]> = any
+type Append<T extends unknown[], U extends unknown[]> = T extends []
+  ? { [I in keyof U]: [U[I]] }
+  : [T, U] extends [
+      [infer X extends unknown[], ...infer RX extends unknown[]],
+      [infer Y, ...infer RY extends unknown[]],
+    ]
+  ? [[...X, Y], ...Append<RX, RY>]
+  : [T, U] extends [
+      [infer X, ...infer RX extends unknown[]],
+      [infer Y, ...infer RY extends unknown[]],
+    ]
+  ? [[X, Y], ...Append<RX, RY>]
+  : [];
+
+type Transpose<M extends number[][], Ans extends unknown[][] = []> = M extends [
+  infer R extends number[],
+  ...infer Rest extends number[][],
+]
+  ? Transpose<Rest, Append<Ans, R>>
+  : Ans;
