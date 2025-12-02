ac9d1ac3923ad2bec583d23ef84ca58d6c9ca9f1
Kawano Kenichi <kenichi.k.820@gmail.com>
2025-12-02 15:29:22 +0000

    ✨ Add an answer, 27152-medium-triangular-number

--- questions/27152-medium-triangular-number/template.ts
+++ questions/27152-medium-triangular-number/template.ts
@@ -1,1 +1,7 @@
-type Triangular<N extends number> = any
+type Triangular<
+  N extends number,
+  C extends readonly 0[] = readonly [],
+  A extends readonly 0[] = readonly [],
+> = C["length"] extends N
+  ? A["length"]
+  : Triangular<N, readonly [...C, 0], readonly [...A, ...C, 0]>;
