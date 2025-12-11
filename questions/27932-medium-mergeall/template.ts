e1d4ff1911738c29c18f0010075e1598a8fd884c
Kawano Kenichi <kenichi.k.820@gmail.com>
2025-12-11 15:32:57 +0000

    ✨ Add an answer, 27932-medium-mergeall

--- questions/27932-medium-mergeall/template.ts
+++ questions/27932-medium-mergeall/template.ts
@@ -1,1 +1,5 @@
-type MergeAll<XS> = any
+type MergeAll<
+  XS extends Record<PropertyKey, unknown>[],
+  U = XS[number],
+  K extends PropertyKey = U extends U ? keyof U : never,
+> = { [P in K]: U extends U ? U[P & keyof U] : never };
