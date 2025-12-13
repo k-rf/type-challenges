c9222c465d72db5dbaf616106bdd69d073bf9595
Kawano Kenichi <kenichi.k.820@gmail.com>
2025-12-13 15:29:38 +0000

    ✨ Add an answer, 27958-medium-checkrepeatedtuple

--- questions/27958-medium-checkrepeatedtuple/template.ts
+++ questions/27958-medium-checkrepeatedtuple/template.ts
@@ -1,1 +1,10 @@
-type CheckRepeatedTuple<T extends unknown[]> = any
+type CheckRepeatedTuple<
+  T extends unknown[],
+  Seen extends unknown[] = [],
+> = T extends [infer H, ...infer R]
+  ? Includes<Seen, H> extends true
+    ? true
+    : CheckRepeatedTuple<R, [...Seen, H]>
+  : IsEqual<T["length"], number> extends true
+  ? false
+  : false;
