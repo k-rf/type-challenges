3bcda192b885bb6e236ce12edbacde0b8b076dc6
Kawano Kenichi <kenichi.k.820@gmail.com>
2025-12-15 14:38:03 +0000

    ✨ Add an answer, 29650-medium-extracttoobject

--- questions/29650-medium-extracttoobject/template.ts
+++ questions/29650-medium-extracttoobject/template.ts
@@ -1,1 +1,7 @@
-type ExtractToObject<T, U> = any
+type Prettify<T> = Omit<T, never>;
+
+type ExtractToObject<T, U extends keyof T> = Prettify<
+  {
+    [K in keyof T as K extends U ? never : K]: T[K];
+  } & T[U]
+>;
