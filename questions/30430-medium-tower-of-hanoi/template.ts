da7fb28806534b2395327d46e5f659752a3c8a9a
Kawano Kenichi <kenichi.k.820@gmail.com>
2025-12-18 17:12:23 +0000

    ✨ Add an answer, 30430-medium-tower-of-hanoi

--- questions/30430-medium-tower-of-hanoi/template.ts
+++ questions/30430-medium-tower-of-hanoi/template.ts
@@ -1,1 +1,13 @@
-type Hanoi<N extends number, From = 'A', To = 'B', Intermediate = 'C'> = any
+type Hanoi<
+  N extends number,
+  From = "A",
+  To = "B",
+  Intermediate = "C",
+  C extends 0[] = [],
+> = N extends C["length"]
+  ? []
+  : [
+      ...Hanoi<N, From, Intermediate, To, [...C, 0]>,
+      [From, To],
+      ...Hanoi<N, Intermediate, To, From, [...C, 0]>,
+    ];
