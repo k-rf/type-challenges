a3e10b8b7e30227eb59683cecedd10ff5bd1bf02
Kawano Kenichi <kenichi.k.820@gmail.com>
2025-12-28 10:49:18 +0000

    ✨ Add an answer, 34007-medium-compare-array-length

--- questions/34007-medium-compare-array-length/template.ts
+++ questions/34007-medium-compare-array-length/template.ts
@@ -1,1 +1,12 @@
-type CompareArrayLength<T extends any[], U extends any[]> = any
+type CompareArrayLength<T extends any[], U extends any[]> = [T, U] extends [
+  [],
+  [],
+]
+  ? 0
+  : T extends []
+  ? -1
+  : U extends []
+  ? 1
+  : [T, U] extends [[unknown, ...infer TR], [unknown, ...infer UR]]
+  ? CompareArrayLength<TR, UR>
+  : never;
