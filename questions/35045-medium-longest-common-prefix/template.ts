6efcb2d5de8deb52786fb25d0c6d521718ae15f4
Kawano Kenichi <kenichi.k.820@gmail.com>
2025-12-31 09:00:34 +0000

    ✨ Add an answer, 35045-medium-longest-common-prefix
    答え参照

--- questions/35045-medium-longest-common-prefix/template.ts
+++ questions/35045-medium-longest-common-prefix/template.ts
@@ -1,1 +1,8 @@
-type LongestCommonPrefix<T extends string[], P extends string = ''> = any
+type LongestCommonPrefix<
+  T extends string[],
+  P extends string = "",
+> = T extends [`${P}${infer Next}${string}`, ...string[]]
+  ? T extends `${P}${Next}${string}`[]
+    ? LongestCommonPrefix<T, `${P}${Next}`>
+    : P
+  : P;
