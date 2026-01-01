47982693ca440da726e4bd38fcfe24b7b1273c53
Kawano Kenichi <kenichi.k.820@gmail.com>
2026-01-01 17:17:56 +0000

    ✨ Add an answer, 35191-medium-trace

--- questions/35191-medium-trace/template.ts
+++ questions/35191-medium-trace/template.ts
@@ -1,1 +1,6 @@
-type Trace<T extends any[][]> = any
+type Trace<
+  T extends any[][],
+  I extends 0[] = [],
+> = I["length"] extends keyof T[I["length"]]
+  ? T[I["length"]][I["length"]] | Trace<T, [...I, 0]>
+  : never;
