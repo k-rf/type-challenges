e477655330a7faf7489de447cd2c3e03cf0aeded
Kawano Kenichi <kenichi.k.820@gmail.com>
2026-01-06 14:31:30 +0000

    ✨ Add an answer, 00017-hard-currying-1

--- questions/00017-hard-currying-1/template.ts
+++ questions/00017-hard-currying-1/template.ts
@@ -1,1 +1,7 @@
-declare function Currying(fn: any): any
+declare function Currying<T>(fn: T): Curried<T>;
+
+type Curried<T> = T extends (...args: infer A) => infer R
+  ? A extends [infer U, ...infer Rest extends [unknown, ...unknown[]]]
+    ? (arg: U) => Curried<(...args: Rest) => R>
+    : T
+  : never;
