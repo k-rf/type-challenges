1a6215e19f1999930617a819f15a4fc3b0394942
Kawano Kenichi <kenichi.k.820@gmail.com>
2026-01-21 14:35:59 +0000

    ✨ Add an answer, 00112-hard-capitalizewords

--- questions/00112-hard-capitalizewords/template.ts
+++ questions/00112-hard-capitalizewords/template.ts
@@ -1,1 +1,21 @@
-type CapitalizeWords<S extends string> = any
+type Cap<
+  S extends string,
+  Sep extends string = " ",
+  Sp extends string[] = Split<S, Sep>,
+> = Join<
+  {
+    [K in keyof Sp]: Capitalize<Sp[K]>;
+  },
+  Sep
+>;
+
+type CapitalizeWords<
+  S extends string,
+  Separators extends string[] = Split<" .,!@#$%^&*()_+{}|🤣", "">,
+> = Separators extends [infer Sep extends string, ...infer R extends string[]]
+  ? CapitalizeWords<Cap<S, Sep>, R>
+  : S;
+
+type Dummy =
+  //   ^? XXX: twoslash-queryを書いておかないとエラーになる
+  CapitalizeWords<"aa!bb@cc#dd$ee%ff^gg&hh*ii(jj)kk_ll+mm{nn}oo|pp🤣qq">;
