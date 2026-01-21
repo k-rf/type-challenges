999e8dd49779ba3f6ac009bc8ef02d3b94a32452
Kawano Kenichi <kenichi.k.820@gmail.com>
2026-01-21 13:56:44 +0000

    ✨ Add an answer, 02822-hard-split

--- questions/02822-hard-split/template.ts
+++ questions/02822-hard-split/template.ts
@@ -1,1 +1,7 @@
-type Split<S extends string, SEP extends string> = any
+type Split<S extends string, SEP extends string = never> = S extends SEP
+  ? []
+  : S extends `${infer X}${SEP}${infer Y}`
+    ? [X, ...(ToSplitted<Y>["length"] extends 0 ? [] : Split<Y, SEP>)]
+    : IsEqual<S, string> extends true
+      ? S[]
+      : [S];
