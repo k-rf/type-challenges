a01e227762ca1ac984c152a9201b7d88007d1aaa
Kawano Kenichi <kenichi.k.820@gmail.com>
2026-01-21 14:42:00 +0000

    🎨 Update an answer, 00112-hard-capitalizewords
    区切り文字は、大文字化と小文字化したときに同じものになるかどうかで判定できる。
    なのでSplitを使う必要はない。

--- questions/00112-hard-capitalizewords/template.ts
+++ questions/00112-hard-capitalizewords/template.ts
@@ -1,21 +1,8 @@
-type Cap<
-  S extends string,
-  Sep extends string = " ",
-  Sp extends string[] = Split<S, Sep>,
-> = Join<
-  {
-    [K in keyof Sp]: Capitalize<Sp[K]>;
-  },
-  Sep
->;
-
 type CapitalizeWords<
   S extends string,
-  Separators extends string[] = Split<" .,!@#$%^&*()_+{}|🤣", "">,
-> = Separators extends [infer Sep extends string, ...infer R extends string[]]
-  ? CapitalizeWords<Cap<S, Sep>, R>
-  : S;
-
-type Dummy =
-  //   ^? XXX: twoslash-queryを書いておかないとエラーになる
-  CapitalizeWords<"aa!bb@cc#dd$ee%ff^gg&hh*ii(jj)kk_ll+mm{nn}oo|pp🤣qq">;
+  W extends string = "",
+> = S extends `${infer A}${infer B}`
+  ? Uppercase<A> extends Lowercase<A>
+    ? `${Capitalize<`${W}${A}`>}${CapitalizeWords<B>}`
+    : CapitalizeWords<B, `${W}${A}`>
+  : Capitalize<W>;
