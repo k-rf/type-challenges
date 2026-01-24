eab7ee77830c8737a96bd90b5ae921a4b107f445
Kawano Kenichi <kenichi.k.820@gmail.com>
2026-01-24 07:45:27 +0000

    ✨ Add an answer, 00147-hard-c-printf-parser

--- questions/00147-hard-c-printf-parser/template.ts
+++ questions/00147-hard-c-printf-parser/template.ts
@@ -1,11 +1,18 @@
 type ControlsMap = {
-  c: 'char'
-  s: 'string'
-  d: 'dec'
-  o: 'oct'
-  h: 'hex'
-  f: 'float'
-  p: 'pointer'
-}
+  c: "char";
+  s: "string";
+  d: "dec";
+  o: "oct";
+  h: "hex";
+  f: "float";
+  p: "pointer";
+};
 
-type ParsePrintFormat = any
+type ParsePrintFormat<
+  S extends string,
+  A extends string[] = [],
+> = S extends `${infer X}%%${infer Y}`
+  ? ParsePrintFormat<`${X}${Y}`, A>
+  : S extends `${string}%${infer U extends keyof ControlsMap}${infer R}`
+    ? ParsePrintFormat<R, [...A, ControlsMap[U]]>
+    : A;
