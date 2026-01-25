33f26ecfec6f042a6418210ab0d9557e3460bc66
Kawano Kenichi <kenichi.k.820@gmail.com>
2026-01-25 02:57:22 +0000

    ✨ Add an answer, 00213-hard-vue-basic-props
    答え参照

--- questions/00213-hard-vue-basic-props/template.ts
+++ questions/00213-hard-vue-basic-props/template.ts
@@ -1,1 +1,30 @@
-declare function VueBasicProps(options: any): any
+type InferComputed<C extends Record<string, any>> = {
+  [K in keyof C]: ReturnType<C[K]>;
+};
+
+type Prop<T = any> = PropType<T> | { type?: PropType<T> };
+type PropType<T> = PropConstructor<T> | PropConstructor<T>[];
+
+type PropConstructor<T = any> =
+  | { new (...args: any[]): T & object }
+  | { (): T };
+
+type InferPropType<P> =
+  P extends Prop<infer T> ? (unknown extends T ? any : T) : any;
+
+type InferProps<P extends Record<string, any>> = {
+  [K in keyof P]: InferPropType<P[K]>;
+};
+
+declare function VueBasicProps<
+  P extends Record<string, any>,
+  D,
+  C extends Record<string, any>,
+  M,
+  Props = InferProps<P>,
+>(options: {
+  props?: P;
+  data: (this: Props) => D;
+  computed: C & ThisType<Props & D & InferComputed<C> & M>;
+  methods: M & ThisType<Props & D & InferComputed<C> & M>;
+}): Props & D & InferComputed<C> & M;
