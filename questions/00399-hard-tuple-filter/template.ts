7c4e64aa23be00d27d04a4d3128418e992c71570
Kawano Kenichi <kenichi.k.820@gmail.com>
2026-01-29 14:44:14 +0000

    ✨ Add an answer, 00399-hard-tuple-filter

--- questions/00399-hard-tuple-filter/template.ts
+++ questions/00399-hard-tuple-filter/template.ts
@@ -1,1 +1,5 @@
-type FilterOut<T extends any[], F> = any
+type FilterOut<T extends readonly any[], F> = T extends [infer U, ...infer R]
+  ? [U] extends [F]
+    ? FilterOut<R, F>
+    : [U, ...FilterOut<R, F>]
+  : [];
