c28cef752297f5cadb6d7af56c12e44ba528875e
Kawano Kenichi <kenichi.k.820@gmail.com>
2026-01-31 13:48:48 +0000

    ✨ Add an answer, 00472-hard-tuple-to-enum-object
    答え参照

--- questions/00472-hard-tuple-to-enum-object/template.ts
+++ questions/00472-hard-tuple-to-enum-object/template.ts
@@ -1,1 +1,10 @@
-type Enum<T extends readonly string[], N extends boolean = false> = any
+type TupleKeys<T extends readonly unknown[]> = T extends readonly [
+  unknown,
+  ...infer Tail,
+]
+  ? TupleKeys<Tail> | Tail["length"]
+  : never;
+
+type Enum<T extends readonly string[], N extends boolean = false> = {
+  readonly [K in TupleKeys<T> as Capitalize<T[K]>]: N extends true ? K : T[K];
+};
