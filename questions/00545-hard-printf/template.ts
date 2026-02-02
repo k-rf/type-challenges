da073e427478a3c923fe4f6e628c1cc0df21292e
Kawano Kenichi <kenichi.k.820@gmail.com>
2026-02-02 05:21:13 +0000

    ✨ Add an answer, 00545-hard-printf

--- questions/00545-hard-printf/template.ts
+++ questions/00545-hard-printf/template.ts
@@ -1,1 +1,7 @@
-type Format<T extends string> = any
+type Arg<T extends "d" | "s"> = { d: number; s: string }[T];
+
+type Format<T extends string> = T extends `${string}%${infer U}${infer R}`
+  ? U extends "d" | "s"
+    ? (arg: Arg<U>) => Format<R>
+    : Format<R>
+  : string;
