349fc79ccdd5eb8aa9bf4dc307179f6e60d9a680
Kawano Kenichi <kenichi.k.820@gmail.com>
2026-02-08 09:33:40 +0000

    ✨ Add an answer, 00956-hard-deeppick

--- questions/00956-hard-deeppick/template.ts
+++ questions/00956-hard-deeppick/template.ts
@@ -1,1 +1,9 @@
-type DeepPick = any
+type DeepPick<O, K> = UnionToIntersection<
+  K extends `${infer U}.${infer R}`
+    ? U extends keyof O
+      ? Record<U, DeepPick<O[U], R>>
+      : never
+    : K extends keyof O
+      ? Record<K, O[K]>
+      : never
+>;
