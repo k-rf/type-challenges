ce610a712d6bbd1addb8b6fb4b21143c2b50220f
Kawano Kenichi <kenichi.k.820@gmail.com>
2026-02-09 13:37:05 +0000

    ✨ Add an answer, 01290-hard-pinia

--- questions/01290-hard-pinia/template.ts
+++ questions/01290-hard-pinia/template.ts
@@ -1,1 +1,12 @@
-declare function defineStore(store: unknown): unknown
+type Getter<T> = {
+  [K in keyof T]: T[K] extends (...args: any) => infer U ? U : never;
+};
+
+type Store<S, G, A> = {
+  id: string;
+  state: () => S;
+  getters: G & ThisType<Readonly<S> & Getter<G>>;
+  actions: A & ThisType<S & A>;
+};
+
+declare function defineStore<S, G, A>(store: Store<S, G, A>): S & Getter<G> & A;
