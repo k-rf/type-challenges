db2b2b828d1e404ecc399b3d5a85d69838d59995
Kawano Kenichi <kenichi.k.820@gmail.com>
2026-02-11 09:48:32 +0000

    ✨ Add an answer, 02059-hard-drop-string

--- questions/02059-hard-drop-string/template.ts
+++ questions/02059-hard-drop-string/template.ts
@@ -1,1 +1,8 @@
-type DropString<S, R> = any
+type DropString<
+  S extends string,
+  R extends string,
+> = S extends `${infer H}${infer T}`
+  ? Includes<ToSplitted<R>, H> extends true
+    ? DropString<T, R>
+    : `${H}${DropString<T, R>}`
+  : S;
