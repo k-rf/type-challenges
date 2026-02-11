9714e22a35c55af423272d5318be31ffa8d43582
Kawano Kenichi <kenichi.k.820@gmail.com>
2026-02-11 09:48:15 +0000

    ✨ Add an answer, 01383-hard-camelize

--- questions/01383-hard-camelize/template.ts
+++ questions/01383-hard-camelize/template.ts
@@ -1,1 +1,9 @@
-type Camelize<T> = any
+type CamelizeArray<T> = T extends [infer U, ...infer R]
+  ? [Camelize<U>, ...CamelizeArray<R>]
+  : T;
+
+type Camelize<T> = {
+  [K in keyof T as K extends string ? CamelCase<K> : K]: T[K] extends any[]
+    ? CamelizeArray<T[K]>
+    : Camelize<T[K]>;
+};
