ef2e3e99875a9e667366196e141d15f952aa139a
Kawano Kenichi <kenichi.k.820@gmail.com>
2026-02-15 07:11:25 +0000

    ✨ Add an answer, 02949-hard-objectfromentries

--- questions/02949-hard-objectfromentries/template.ts
+++ questions/02949-hard-objectfromentries/template.ts
@@ -1,1 +1,4 @@
-type ObjectFromEntries<T> = any
+type ObjectFromEntries<T> =
+  UnionToTuple<T> extends [infer H extends [string, any], ...any]
+    ? Prettify<{ [key in H[0]]: H[1] } & ObjectFromEntries<Exclude<T, H>>>
+    : {};
