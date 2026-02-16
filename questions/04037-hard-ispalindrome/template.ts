e3c43e1833a93310efea14c272983f91f107d52e
Kawano Kenichi <kenichi.k.820@gmail.com>
2026-02-16 15:16:30 +0000

    ✨ Add an answer, 04037-hard-ispalindrome

--- questions/04037-hard-ispalindrome/template.ts
+++ questions/04037-hard-ispalindrome/template.ts
@@ -1,1 +1,12 @@
-type IsPalindrome<T> = any
+type IsPalindrome<
+  T extends string | number,
+  S extends string[] = ToSplitted<`${T}`>,
+> = S extends [
+  infer U extends string,
+  ...infer V extends string[],
+  infer U extends string,
+]
+  ? IsUnion<U> extends true
+    ? false
+    : IsPalindrome<Join<V, "">>
+  : true;
