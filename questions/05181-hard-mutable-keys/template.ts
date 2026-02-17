477b9d2220d349c5992f5793eef8487d761adb46
Kawano Kenichi <kenichi.k.820@gmail.com>
2026-02-17 16:44:25 +0000

    ✨ Add an answer, 05181-hard-mutable-keys

--- questions/05181-hard-mutable-keys/template.ts
+++ questions/05181-hard-mutable-keys/template.ts
@@ -1,1 +1,5 @@
-type MutableKeys<T> = any
+type MutableKeys<T extends Record<PropertyKey, unknown>> = keyof {
+  [K in keyof T as IsEqual<Pick<T, K>, Readonly<Pick<T, K>>> extends true
+    ? never
+    : K]: any;
+};
