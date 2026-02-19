ae72adec37c7bdfff14502d6d02cd1410c682c89
Kawano Kenichi <kenichi.k.820@gmail.com>
2026-02-19 16:39:09 +0000

    ✨ Add an answer, 06141-hard-binary-to-decimal
    答え参照

--- questions/06141-hard-binary-to-decimal/template.ts
+++ questions/06141-hard-binary-to-decimal/template.ts
@@ -1,1 +1,8 @@
-type BinaryToDecimal<S extends string> = any
+type BinaryToDecimal<
+  S extends string,
+  A extends any[] = [],
+> = S extends `${infer U}${infer R}`
+  ? U extends "0"
+    ? BinaryToDecimal<R, [...A, ...A]>
+    : BinaryToDecimal<R, [...A, ...A, 1]>
+  : A["length"];
