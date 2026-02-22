863e68dda70ca2be4baba003de1eba54c5e96cf5
Kawano Kenichi <kenichi.k.820@gmail.com>
2026-02-22 05:12:27 +0000

    ✨ Add an answer, 08804-hard-two-sum

--- questions/08804-hard-two-sum/template.ts
+++ questions/08804-hard-two-sum/template.ts
@@ -1,1 +1,12 @@
-type TwoSum<T extends number[], U extends number> = any
+type AddPattern<A extends number, B extends number> = B extends any
+  ? Add<A, B>
+  : never;
+
+type TwoSum<T extends number[], U extends number> = T extends [
+  infer F extends number,
+  ...infer R extends number[],
+]
+  ? U extends AddPattern<F, R[number]>
+    ? true
+    : TwoSum<R, U>
+  : false;
